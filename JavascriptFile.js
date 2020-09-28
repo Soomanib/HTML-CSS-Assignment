6ecbf5b6105043aaaee89b89782ba862dc145608
ENOSISBD\soomanib <soomanib.kamruzzaman@enosisbd.com>
2020-09-28 11:17:53 +0600

    committed javascript edit feature

--- JavascriptFile.js
+++ JavascriptFile.js
@@ -2,14 +2,21 @@ document.getElementById("FormSubmitButton").addEventListener( "click", formSubmi
 
 document.getElementById("AddButton").addEventListener( "click", addButtonClicked );
 
-document.getElementById("SortByID").addEventListener( "click", sortTableByID );
+document.getElementById("SortByID").addEventListener( "click", function(){
+    sortTable( 0, "asc" );
+} );
 
-document.getElementById("SortByDayCount").addEventListener( "click", sortTableByDayCount );
+document.getElementById("SortByIdDesc").addEventListener( "click",  function(){
+    sortTable( 0, "desc" );
+} );
 
-document.getElementById("SortByIdDesc").addEventListener( "click", sortTableByIdDesc);
+document.getElementById("SortByDayCount").addEventListener( "click", function(){
+    sortTable( 5, "asc" );
+} );
 
-document.getElementById("SortByDayCountDesc").addEventListener( "click", sortTableByDayCountDesc );
-
+document.getElementById("SortByDayCountDesc").addEventListener( "click", function(){
+    sortTable( 5, "desc" );
+} );
 
 
 function formSubmitClicked()
@@ -24,14 +31,50 @@ function formSubmitClicked()
     }
 }
 
+function getEmployeeInformation()
+{
+    let intialStoreArray = [];
+    let employeeInformationArray = [];
+    let formData = document.getElementById("RegistrationFormContent");
+
+    for(let i=0; i<=8; i++)
+    {
+        intialStoreArray[i] = formData.elements[i].value;
+    }
+
+    employeeInformationArray[0] = intialStoreArray[1]; //id
+    employeeInformationArray[1] = intialStoreArray[0]; //name
+    employeeInformationArray[2] = intialStoreArray[2]; //email
+    employeeInformationArray[3] = intialStoreArray[3]; //start_date
+    employeeInformationArray[4] = intialStoreArray[4]; //end_date
+    employeeInformationArray[5] = date_diff_indays(intialStoreArray[3], intialStoreArray[4]); //day_count
+
+    employeeInformationArray[7] = intialStoreArray[8]; //file
+
+    const leaveSelectors = document.querySelectorAll( 'input[name="leave-type"]' );
+
+    let leaveSelectedValue;
+
+    for(leaveSelector of leaveSelectors)
+    {
+        if(leaveSelector.checked)
+        {
+            leaveSelectedValue = leaveSelector.value;
+        }
+    }
+
+    employeeInformationArray[6] = leaveSelectedValue;
+
+    return employeeInformationArray;
+}
+
 function formValidated(employeeInformation) {
-    let message = "These fields cannot be empty";
-    let formValidationSection = document.getElementById("ValidateForm");
+    let message = "These field(s) cannot be empty: ";
 
     let isValid = true;
     for(let i=0; i<8; i++)
     {
-        if(employeeInformation[i] == "")
+        if( !( employeeInformation[i] ) )
         {
             isValid = false;
             switch(i)
@@ -55,7 +98,7 @@ function formValidated(employeeInformation) {
                     message += " Leave Type,"
                 break;
                 case 7: 
-                    message += " File"
+                    message += " File,"
                 break;
             }
         }
@@ -63,43 +106,14 @@ function formValidated(employeeInformation) {
 
     if(!isValid)
     {
-        alert(message);
+        let alertMessage = message.slice(0, -1);
+        alert(alertMessage);
     }
 
     return isValid;
 }
 
-function getEmployeeInformation()
-{
-    let intialStoreArray = [];
-    let employeeInformationArray = [];
-    let formData = document.getElementById("RegistrationFormContent");
 
-    for(let i=0; i<=8; i++)
-    {
-        intialStoreArray[i] = formData.elements[i].value;
-    }
-
-    employeeInformationArray[0] = intialStoreArray[1]; //id
-    employeeInformationArray[1] = intialStoreArray[0]; //name
-    employeeInformationArray[2] = intialStoreArray[2]; //email
-    employeeInformationArray[3] = intialStoreArray[3]; //start_date
-    employeeInformationArray[4] = intialStoreArray[4]; //end_date
-    employeeInformationArray[5] = date_diff_indays(intialStoreArray[3], intialStoreArray[4]); //day_count
-
-    employeeInformationArray[7] = intialStoreArray[8]; //file
-
-    if(intialStoreArray[6]=="on") //radio-button
-    {
-        employeeInformationArray[6] = "Casual";
-    }
-    else
-    {
-        employeeInformationArray[6] = "Sick";
-    }
-
-    return employeeInformationArray;
-}
 
 function addButtonClicked()
 {
@@ -144,6 +158,7 @@ function createRow(employeeInformation)
                 deleteButton.classList.add("edit-delete-button");  
 
                 deleteButton.setAttribute("onclick", "deleteFormEntry(this)");
+                editButton.setAttribute("onclick", "editFormEntry(this)");
             }
 
             else{
@@ -154,9 +169,107 @@ function createRow(employeeInformation)
 
 function deleteFormEntry(deleteButton)
 {
-    var row = deleteButton.parentNode.parentNode;
+    let row = deleteButton.parentNode.parentNode;
     row.parentNode.removeChild(row);
 }
+
+
+function editFormEntry(editButton){
+    let row = editButton.parentNode.parentNode;
+    let callerFunction;
+
+    //change form header text
+    document.getElementById("LeaveTableHeadText").innerHTML = "Edit Leave Information";
+
+    //show form, don't show list page
+    document.getElementById("ListContainer").style.display = "none";
+    document.getElementById("RegistrationFormContainer").style.display = "flex";
+
+    //change event listener for save button
+    document.getElementById("FormSubmitButton").removeEventListener( "click", formSubmitClicked );
+    document.getElementById("FormSubmitButton").addEventListener( "click", callerFunction = function(){
+        editSubmitClicked(row, callerFunction);
+    } );
+
+    placeHolder(row);
+}
+
+
+function placeHolder(row){
+
+    let formData = document.getElementById("RegistrationFormContent");
+    let intialStoreArray = [];
+    let imageFile;
+    let filePath;
+    let fileName;
+
+    for(i=0; i<7; i++)
+    {
+        intialStoreArray[i] = row.getElementsByTagName("TD")[i].innerHTML;
+    }
+
+    formData.elements[0].value = intialStoreArray[1]; //name
+    formData.elements[1].value = intialStoreArray[0]; //id
+    formData.elements[2].value = intialStoreArray[2]; //email
+    formData.elements[3].value = intialStoreArray[3]; //start_date
+    formData.elements[4].value = intialStoreArray[4]; //end_date
+
+    imageFile = row.getElementsByTagName("TD")[7].childNodes[0];
+    filePath = imageFile.src;
+    fileName = filePath.split('\\').pop().split('/').pop();
+
+    //file do it later
+    //formData.elements[8].value = fileName; //file
+
+    //radio-button
+    if(intialStoreArray[6] == "Sick")
+    {
+        document.getElementById("SickLeaveType").checked = true;
+    }
+    else if(intialStoreArray[6] == "Custom")
+    {
+        document.getElementById("CustomLeaveType").checked = true;
+    }
+}
+
+function editSubmitClicked(row, callerFunction){
+
+    //change row values
+    //get employee information from form
+    let employeeInformation = getEmployeeInformation();
+    let columnData;
+    let filePath;
+
+    //check if form is validated
+
+    if( formValidated(employeeInformation) ){
+        for(i=0; i<7; i++)
+        {
+            columnData = row.getElementsByTagName("TD")[i];
+            columnData.innerHTML = employeeInformation[i];
+        }
+
+        columnData = row.getElementsByTagName("TD")[7];
+        filePath = employeeInformation[7].split('\\').pop().split('/').pop();
+        columnData.childNodes[0].src = filePath;
+
+        //Head information back to default
+        document.getElementById("LeaveTableHeadText").innerHTML = "Register For New Leave";
+        
+        // changed parameters to default value 
+        //show leave content and remove flex
+
+        document.getElementById("ListContainer").style.display = "flex";
+        document.getElementById("RegistrationFormContainer").style.display = "none";
+
+        //get back to original event listener
+        document.getElementById("FormSubmitButton").addEventListener( "click", formSubmitClicked );
+        document.getElementById("FormSubmitButton").removeEventListener( "click", callerFunction );
+
+        document.getElementById("RegistrationFormContent").reset();
+    }
+}
+
 
 let date_diff_indays = function(date1, date2) {
     dt1 = new Date(date1);
@@ -166,10 +279,10 @@ let date_diff_indays = function(date1, date2) {
 
 
 
+//This is sort table , n=0 means by id, n=5 means by day count
+//mode can be "asc" or "desc"
 
-//This is sort table by id asc
-
-function sortTableByID() {
+function sortTable( n, mode ) {
     let table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("LeaveTableRowContainer");
     switching = true;
@@ -183,13 +296,20 @@ function sortTableByID() {
 
         shouldSwitch = false;
 
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
 
-
-        if (Number(x.innerHTML) > Number(y.innerHTML)) {
-            shouldSwitch = true;
-            break;
+        if(mode == "asc"){
+            if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        else if(mode == "desc"){
+            if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                shouldSwitch = true;
+                break;
+            }
         }
       }
 
@@ -201,110 +321,6 @@ function sortTableByID() {
     }
   }
 
-
-
-  //This is sort table by days count, mode is ascending
-
-
-  function sortTableByDayCount() {
-    let table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("LeaveTableRowContainer");
-    switching = true;
-
-    while (switching) {
-
-      switching = false;
-      rows = table.rows;
-
-      for (i = 0; i < (rows.length - 1); i++) {
-
-        shouldSwitch = false;
-
-        x = rows[i].getElementsByTagName("TD")[5];
-        y = rows[i + 1].getElementsByTagName("TD")[5];
-
-        if (Number(x.innerHTML) > Number(y.innerHTML)) {
-            shouldSwitch = true;
-            break;
-        }
-      }
-
-      if (shouldSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
-  }
-
-
-//This is sort table by id desc
-
-function sortTableByIdDesc() {
-    let table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("LeaveTableRowContainer");
-    switching = true;
-
-    while (switching) {
-
-      switching = false;
-      rows = table.rows;
-
-      for (i = 0; i < (rows.length - 1); i++) {
-
-        shouldSwitch = false;
-
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
-
-
-        if (Number(x.innerHTML) < Number(y.innerHTML)) {
-            shouldSwitch = true;
-            break;
-        }
- 
-
-      }
-      if (shouldSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
-  }
-
-
-
-//This is sort table by days count, mode is descending
-
-
-function sortTableByDayCountDesc() {
-let table, rows, switching, i, x, y, shouldSwitch;
-table = document.getElementById("LeaveTableRowContainer");
-switching = true;
-
-while (switching) {
-
-    switching = false;
-    rows = table.rows;
-
-    for (i = 0; i < (rows.length - 1); i++) {
-
-        shouldSwitch = false;
-
-        x = rows[i].getElementsByTagName("TD")[5];
-        y = rows[i + 1].getElementsByTagName("TD")[5];
-
-        if (Number(x.innerHTML) < Number(y.innerHTML)) {
-            shouldSwitch = true;
-            break;
-        }
-    }
-    
-    if (shouldSwitch) {
-    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-    switching = true;
-    }
-}
-}
 
 //search function
 
